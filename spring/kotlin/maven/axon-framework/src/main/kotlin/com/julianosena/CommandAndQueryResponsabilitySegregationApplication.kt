e99e92e61fec0176064286115fa86f7fc1802b37
Juliano Sena da Silva Carlos <julianossc@gmail.com>
2022-03-20 15:34:57 +0000

    O tempo na webapplication acabou, n deu nem tempo de fazer o mapeamento

---
+++ spring/kotlin/maven/axon-framework/src/main/kotlin/com/julianosena/CommandAndQueryResponsabilitySegregationApplication.kt
@@ -0,0 +1,11 @@
+package com.julianosena
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommandAndQueryResponsabilitySegregationApplication
+
+fun main(args: Array<String>) {
+	runApplication<CommandAndQueryResponsabilitySegregationApplication>(*args)
+}
---
+++ spring/kotlin/maven/axon-framework/src/test/kotlin/com/julianosena/CommandAndQueryResponsabilitySegregationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.julianosena
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommandAndQueryResponsabilitySegregationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
